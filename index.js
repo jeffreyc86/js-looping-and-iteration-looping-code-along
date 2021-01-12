c11d53c3ec71413811de5cadfa7d5039753ee280
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-01-11 21:47:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (array, string) {
+    let arr = []
+    for (let i = 0; i < array.length; i++) {
+        arr.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+    return arr
+}
+
+function countDown(num){
+    let i = 0
+    while (num >= i){
+        console.log(num--)
+    }
+}
